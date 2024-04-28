8c49be005e258f64d34cf87f5db1dd2ff2103799
Sai Inampudi <sai.inampudi@gmail.com>
2024-04-27 21:40:35 -0500

    Initial stab at zipcode search POST route, not working yet

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,27 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
 import { ZipcodeDto } from './zipcode.dto';
 import { ZipcodePipe } from './zipcode.pipe';
 import { ZipcodeService } from './zipcode.service';
+import { ZipcodeSearchDto } from './zipcodeSearch.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly zipcodeService: ZipcodeService) {}
 
   @Get('/zipcodes/:zipcode')
-  async getZipcode(@Param('zipcode', new ZipcodePipe()) id, @Res() res): Promise<ZipcodeDto> {
-      return res.json(await this.zipcodeService.getZipcode(id));
+  async getZipcode(
+    @Param('zipcode', new ZipcodePipe()) id,
+    @Res() res,
+  ): Promise<ZipcodeDto> {
+    return res.json(await this.zipcodeService.getZipcode(id));
+  }
+
+  @Post('/zipcodes')
+  async getTop3MatchingCities(
+    @Body() body: { city: string },
+    @Res() res,
+  ): Promise<ZipcodeSearchDto[]> {
+    const { city } = body;
+    return res.json(await this.zipcodeService.getByTop3MatchingCities(city));
   }
 }
